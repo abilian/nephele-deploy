c12d6384c222d3f0648acceb692239e0e21557b3
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-06-03 16:03:15 +0200

    fix: restart services

--- deploy-mk8s.py
+++ deploy-mk8s.py
@@ -7,13 +7,30 @@ pyinfra -y -vvv --user USER HOST deploy-mk8s.py
 
 from pyinfra import host, logger
 from pyinfra.facts.server import LsbRelease
-from pyinfra.operations import apt, server, snap
+from pyinfra.operations import apt, server, snap, systemd
+
+START_SERVICES = [
+    "containerd",
+    "docker",
+    "redis-server",
+    "snap.lxd.activate",
+    "snap.microk8s.daemon-apiserver-kicker",
+    "snap.microk8s.daemon-apiserver-proxy",
+    "snap.microk8s.daemon-cluster-agent",
+    "snap.microk8s.daemon-containerd",
+    "snap.microk8s.daemon-etcd",
+    "snap.microk8s.daemon-flanneld",
+    "snap.microk8s.daemon-k8s-dqlite",
+    "snap.microk8s.daemon-kubelite",
+]
 
 
 def main() -> None:
     check_server()
     install_apt_packages()
     install_mk8s()
+    start_services()
+    show_status()
     dump_mk8s_config()
 
 
@@ -40,11 +57,24 @@ def install_mk8s():
     """Install microk8s via snap"""
     snap.package(
         name="Install snap packages",
-        packages=["microk8s", "prometheus"],
+        packages=["lxd", "microk8s", "prometheus"],
         classic=True,
         _sudo=True,
     )
 
+
+def start_services() -> None:
+    for service in START_SERVICES:
+        systemd.service(
+            name=f"enable/start {service}",
+            service=service,
+            running=True,
+            enabled=True,
+            _sudo=True,
+        )
+
+
+def show_status():
     server.shell(
         name="Show microk8s status",
         commands=[
