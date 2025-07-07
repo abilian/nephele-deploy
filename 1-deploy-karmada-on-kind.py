bcdfb10f39db0ec0034cac18add0711f94bb3a14
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-07 17:04:36 +0200

    fix karmada initialisation with a retry loop

--- 1-deploy-karmada-on-kind.py
+++ 1-deploy-karmada-on-kind.py
@@ -6,12 +6,9 @@ pyinfra -y -vv --user root HOST 1-deploy-root-kind-k8s.py
 
 from pyinfra import host
 from pyinfra.facts.files import File
-from pyinfra.operations import apt, files, git, server, snap, systemd
+from pyinfra.operations import apt, files, server, snap, systemd
 
 from common import check_server
-from constants import KARMADA_RELEASE_BRANCH
-
-GITS = "/root/git"
 
 APT_PACKAGES = ["curl", "wget", "tar", "gnupg", "vim", "snapd"]
 SNAP_PACKAGES = ["lxd"]
@@ -166,6 +163,12 @@ def create_kind_karmada_cluster():
             "kubectl cluster-info --kubeconfig ~/.kube/config",
         ],
     )
+    server.shell(
+        name="wait status ready",
+        commands=[
+            "kubectl wait --for=condition=Ready pods --all -n kube-system --timeout=300s"
+        ],
+    )
 
 
 def init_karmada_configuration():
@@ -174,20 +177,46 @@ def init_karmada_configuration():
         f"https://github.com/karmada-io/karmada/releases/download/{VERSION}/crds.tar.gz"
     )
     LOG = "/root/log_karmada_init.txt"
+    RETRY = 10
+    WAIT = 10
     server.shell(
-        name="initialize karmada configuration",
+        name="remove old karmada configuration",
+        commands="kubectl karmada deinit --purge-namespace || true",
+        # "rm -fr /var/lib/karmada-etcd ",
+    )
+    server.shell(
+        name="install karmada configuration",
         commands=[
-            "kubectl karmada deinit",
-            "sleep 10",
-            f"kubectl karmada --kubeconfig ~/.kube/config init --crds {CRDS}",
-            "cp -f /etc/karmada/karmada-apiserver.config ~/.kube/",
+            f"""
+            echo "" > {LOG}
+            count=0
+            until grep -q 'installed successfully' {LOG} || [ "$count" -ge "{RETRY}" ]
+            do
+            sleep {WAIT}
+            kubectl karmada --kubeconfig ~/.kube/config init --wait-component-ready-timeout 60 --crds {CRDS} 2>&1 | tee {LOG}
+            count=$((count + 1))
+            done
+            echo count
+            cp -f /etc/karmada/karmada-apiserver.config ~/.kube/
+            """,
+            f"grep -q 'installed successfully' {LOG} ",
         ],
         _get_pty=True,
+    )
+    server.shell(
+        name="check install is successful",
+        commands=f"grep 'installed successfully' {LOG}",
     )
     server.shell(
         name="show cluster info",
         commands=[
             "kubectl cluster-info --kubeconfig ~/.kube/config",
+        ],
+    )
+    server.shell(
+        name="show pods of karmada-system",
+        commands=[
+            "kubectl get pods -n karmada-system",
         ],
     )
 
