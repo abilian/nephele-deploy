7325f51cf9f2577c18d327c0849894d83255fb68
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-07-09 13:54:09 +0200

    mount all images

--- 8-mount-bxl-demo-local-kind.py
+++ 8-mount-bxl-demo-local-kind.py
@@ -19,32 +19,37 @@ KUBECONFIG = "/root/.kube/karmada-apiserver.config"
 LOCAL_IP = "127.0.0.1"
 APP_CLUSTER = "bxl-cluster"
 APP_CONTEXT = "kind-bxl-cluster"
+
+APP_NAMES = ["image-detection", "noise-reduction", "custom-vo"]
 IMAGES = [
     "127.0.0.1:5000/image-detection",
     "127.0.0.1:5000/noise-reduction",
     "127.0.0.1:5000/custom-vo",
 ]
-IMG1_DEPLOY_FILE = "img1-deploy.yaml"
-IMG1_DEPLOY = """\
+# IMG1_DEPLOY_FILE = "image-detection-deploy.yaml"
+# IMG2_DEPLOY_FILE = "noise-reduction-deploy.yaml"
+# IMG3_DEPLOY_FILE = "custom-vo-deploy.yaml"
+
+DEPLOY_TEMPLATE = """\
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: image-detection-deployment
+  name: __name__-deployment
   labels:
-    app: image-detection
+    app: __name__
 spec:
   replicas: 1
   selector:
     matchLabels:
-      app: image-detection
+      app: __name__
   template:
     metadata:
       labels:
-        app: image-detection
+        app: __name__
     spec:
       containers:
-      - name: image-detection-container
-        image: 127.0.0.1:5000/image-detection
+      - name: __name__-container
+        image: 127.0.0.1:5000/__name__
         imagePullPolicy: Never
         ports:
         - containerPort: 8080
@@ -61,50 +66,54 @@ def main() -> None:
 
 
 def put_deploy1_config_file() -> None:
-    files.put(
-        name=f"Put file {IMG1_DEPLOY_FILE!r}",
-        src=io.StringIO(IMG1_DEPLOY),
-        dest=f"/root/{IMG1_DEPLOY_FILE}",
-    )
+    for name in APP_NAMES:
+        deploy_yaml = DEPLOY_TEMPLATE.replace("__name__", name)
+        filename = f"{name}-deploy.yaml"
+        files.put(
+            name=f"Put file {filename!r}",
+            src=io.StringIO(deploy_yaml),
+            dest=f"/root/{filename}",
+        )
 
 
 def kind_load_docker_image() -> None:
-    IMAGES = [
-        "127.0.0.1:5000/image-detection",
-        "127.0.0.1:5000/noise-reduction",
-        "127.0.0.1:5000/custom-vo",
-    ]
-    server.shell(
-        name="kind load docker-image",
-        commands=[f"kind load docker-image {IMAGES[0]} -n {APP_CLUSTER}"],
-    )
+    for name in APP_NAMES:
+        image_name = f"127.0.0.1:5000/{name}"
+        server.shell(
+            name=f"kind load docker-image {image_name}",
+            commands=[f"kind load docker-image {image_name} -n {APP_CLUSTER}"],
+        )
 
 
 def apply_deploy_yaml() -> None:
-    server.shell(
-        name="Apply deploy img1 yaml",
-        commands=[
-            f"kubectl apply -f {IMG1_DEPLOY_FILE} --context {APP_CONTEXT}",
-        ],
-    )
+    for name in APP_NAMES:
+        filename = f"{name}-deploy.yaml"
+        server.shell(
+            name=f"Apply deploy {filename}",
+            commands=[
+                f"kubectl apply -f {filename} --context {APP_CONTEXT}",
+            ],
+        )
 
 
 def check_deployed() -> None:
-    server.shell(
-        name="Check deployed img1",
-        commands=[
-            f"kubectl get pods -l app=image-detection --context {APP_CONTEXT}",
-        ],
-    )
+    for name in APP_NAMES:
+        server.shell(
+            name=f"Check deployed {name!r}",
+            commands=[
+                f"kubectl get pods -l app={name} --context {APP_CONTEXT}",
+            ],
+        )
 
 
 def show_logs_container() -> None:
-    server.shell(
-        name="Show logs container img1",
-        commands=[
-            f"sleep 5 && kubectl logs -l app=image-detection --context {APP_CONTEXT}",
-        ],
-    )
+    for name in APP_NAMES:
+        server.shell(
+            name=f"Show logs container {name!r}",
+            commands=[
+                f"sleep 5 && kubectl logs -l app={name} --context {APP_CONTEXT}",
+            ],
+        )
 
 
 main()
