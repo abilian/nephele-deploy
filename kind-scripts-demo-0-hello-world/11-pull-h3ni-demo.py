81551a351dbf28a537e546321efe6a51afb47d71
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-08-07 12:16:22 +0200

    complete /kind-scripts-demo-0-hello-world (no result)

--- kind-scripts-demo-0-hello-world/11-pull-h3ni-demo.py
+++ kind-scripts-demo-0-hello-world/11-pull-h3ni-demo.py
@@ -12,24 +12,49 @@ from pyinfra.operations import files, server
 
 from constants import GITS
 
+
+SMO_MONOREPO_URL = (
+    "https://gitlab.eclipse.org/eclipse-research-labs/"
+    "nephele-project/opencall-2/h3ni/smo-monorepo.git"
+)
+SMO_DIR = "smo-monorepo"
 DEMO_DIR = "h3ni-demos"
 DEMO_URL = (
     "https://gitlab.eclipse.org/eclipse-research-labs/"
     "nephele-project/opencall-2/h3ni/h3ni-demos.git"
 )
 REPO = f"{GITS}/{DEMO_DIR}"
+SMO_REPO = f"{GITS}/{SMO_DIR}"
 
 
 def main() -> None:
+    make_git_directory()
+    pull_smo_monorepo()
     pull_h3ni_demo()
 
 
-def pull_h3ni_demo() -> None:
+def make_git_directory() -> None:
     files.directory(
         name=f"Create {GITS} directory",
         path=GITS,
     )
 
+
+def pull_smo_monorepo() -> None:
+    server.shell(
+        name=f"Clone/pull smo-monorepo source",
+        commands=[
+            f"[ -d {SMO_REPO} ] || git clone {SMO_MONOREPO_URL} {SMO_REPO}",
+            f"""
+            cd {SMO_REPO}
+            git clean -fxd
+            git pull
+            """,
+        ],
+    )
+
+
+def pull_h3ni_demo() -> None:
     server.shell(
         name=f"Clone/pull {DEMO_DIR} source",
         commands=[
--- kind-scripts-demo-0-hello-world/12-deploy-demo-hello-world.py
+++ kind-scripts-demo-0-hello-world/12-deploy-demo-hello-world.py
@@ -3,7 +3,7 @@ Minimal recipe build for the hello-world app.
 
 Warning: connection as user root.
 
-pyinfra -y -vvv --user root ${SERVER_NAME} 12-deploy-demo-hello-world.py
+pyinfra -y -v --user root ${SERVER_NAME} 12-deploy-demo-hello-world.py
 """
 
 from pyinfra.operations import server, python
@@ -12,63 +12,156 @@ from common import log_callback
 from constants import GITS
 
 TOP_DIR = f"{GITS}/h3ni-demos/"
-SMO_URL = "http://localhost:8000"
+SMO_URL = "http://127.0.0.1:8000"
+REGISTRY_URL = "http://host.docker.internal:5000"
+DEMO = f"{GITS}/h3ni-demos/0-hello-world-demo"
+PROJECT = "demo0"
+GRAPH_NAME = "hello-world-graph"
 
 
-def graph_delete_cmd(graph: str) -> str:
-    return f'curl -X "DELETE" {SMO_URL}/graphs/{graph}'
+def make_graph_delete_cmd(graph: str) -> str:
+    return f"curl -X DELETE {SMO_URL}/graphs/{graph}"
+
+
+def make_deploy_command(project: str, graph: str) -> str:
+    data = f'{{"artifact": "{REGISTRY_URL}/{project}/{graph}"}}'
+    cmd = (
+        f'curl -X POST "{SMO_URL}/project/{project}/graphs" '
+        '-H "Content-Type: application/json" '
+        f"--data '{data}'"
+    )
+    return cmd
+
+
+def make_graph_list_command(project: str) -> str:
+    return f'curl -X GET "{SMO_URL}/project/{project}/graphs"'
+
+
+def make_clusters_list_command() -> str:
+    return f'curl -X GET "{SMO_URL}/clusters/"'
 
 
 def main() -> None:
-    install_hello_world()
+    clean_installed_graphs()
+    prepare_hello_world()
+    deploy_on_smo()
+    check_graph_list()
+    check_clusters()
+    find_kubernetes_pods()
 
 
-def install_hello_world():
+def clean_installed_graphs() -> None:
     for name in ("hello-world-graph", "image-detection-graph"):
-        cmd = graph_delete_cmd(name)
+        cmd = make_graph_delete_cmd(name)
         server.shell(
-            name="Remove prior known graphes",
+            name=f"Remove prior known graph {name}",
             # commands=[(f"yes | {SMO_CLI} graph remove {name} || true")],
             commands=[f"{cmd} || true"],
             _get_pty=True,
+            _shell_executable="/bin/bash",
         )
+
+
+def prepare_hello_world() -> None:
     server.shell(
-        name="Deploy hello-world-graph",
+        name="Prepare hello-world-graph",
         commands=[
             (
                 f"""
                 cd {TOP_DIR}
                 . .venv/bin/activate
-                
-                cd 0-hello-world-demo
-                inv all
+
+                cd {DEMO}
+                inv prepare
                 """
             )
         ],
         _get_pty=True,
+        _shell_executable="/bin/bash",
     )
 
+
+def deploy_on_smo() -> None:
+    cmd = make_deploy_command(PROJECT, GRAPH_NAME)
     result = server.shell(
-        name="Find all kubernetes pods",
-        commands=["kubectl get pods -A"],
+        name="Deploy hello-world-graph to smo",
+        commands=[cmd],
         _get_pty=True,
+        _shell_executable="/bin/bash",
     )
     python.call(
-        name="Show all kubernetes pods",
+        name="Show deploying hello-world-graph",
         function=log_callback,
         result=result,
     )
 
+
+def check_graph_list() -> None:
+    cmd = make_graph_list_command(PROJECT)
     result = server.shell(
         name="Get graph list",
-        commands=[f"{SMO_CLI} graph list"],
+        commands=[cmd],
         _get_pty=True,
+        _shell_executable="/bin/bash",
     )
     python.call(
         name="Show graph list",
         function=log_callback,
         result=result,
     )
+
+
+def check_clusters() -> None:
+    cmd = make_clusters_list_command()
+    result = server.shell(
+        name="Get clusters list",
+        commands=[cmd],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show clusters list",
+        function=log_callback,
+        result=result,
+    )
+
+
+def find_kubernetes_pods() -> None:
+    for context in (
+        "karmada-host",
+        "karmada-apiserver",
+        "member1",
+        "member2",
+    ):
+        result = server.shell(
+            name=f"Find kubernetes pods in {context!r}",
+            commands=[
+                f"""
+            export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+            kubectl config use-context {context}
+
+            kubectl get pods -A
+            """
+            ],
+            _get_pty=True,
+            _shell_executable="/bin/bash",
+        )
+        python.call(
+            name=f"Show kubernetes pods in {context!r}",
+            function=log_callback,
+            result=result,
+        )
+
+    # result = server.shell(
+    #     name="Get graph list",
+    #     commands=[f"{SMO_CLI} graph list"],
+    #     _get_pty=True,
+    # )
+    # python.call(
+    #     name="Show graph list",
+    #     function=log_callback,
+    #     result=result,
+    # )
 
 
 main()
