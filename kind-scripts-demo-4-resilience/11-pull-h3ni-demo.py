cd211d0f7e7209f2de9a382661a90c02ff83255b
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-08-12 16:48:42 +0200

    add scripts kind-scripts-demo-4 (wip, need smo-cli)

---
+++ kind-scripts-demo-4-resilience/11-pull-h3ni-demo.py
@@ -0,0 +1,90 @@
+"""
+Minimal recipe to pull the git h3ni-demo
+
+Warning:
+    - assuming server already configured with git.
+    - connection as user root.
+
+pyinfra -y -vvv --user root ${SERVER_NAME} 11-pull-h3ni-demo.py
+"""
+
+from pyinfra.operations import files, server
+
+from constants import GITS
+
+SMO_MONOREPO_URL = (
+    "https://gitlab.eclipse.org/eclipse-research-labs/"
+    "nephele-project/opencall-2/h3ni/smo-monorepo.git"
+)
+SMO_DIR = "smo-monorepo"
+DEMO_DIR = "h3ni-demos"
+DEMO_URL = (
+    "https://gitlab.eclipse.org/eclipse-research-labs/"
+    "nephele-project/opencall-2/h3ni/h3ni-demos.git"
+)
+REPO = f"{GITS}/{DEMO_DIR}"
+SMO_REPO = f"{GITS}/{SMO_DIR}"
+
+
+def main() -> None:
+    make_git_directory()
+    pull_smo_monorepo()
+    pull_h3ni_demo()
+
+
+def make_git_directory() -> None:
+    files.directory(
+        name=f"Create {GITS} directory",
+        path=GITS,
+    )
+
+
+def pull_smo_monorepo() -> None:
+    server.shell(
+        name="Clone/pull smo-monorepo source",
+        commands=[
+            f"[ -d {SMO_REPO} ] || git clone {SMO_MONOREPO_URL} {SMO_REPO}",
+            f"""
+            cd {SMO_REPO}
+            git clean -fxd
+            git reset --hard HEAD
+            git pull
+            """,
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+
+
+def pull_h3ni_demo() -> None:
+    server.shell(
+        name=f"Clone/pull {DEMO_DIR} source",
+        commands=[
+            f"[ -d {REPO} ] || git clone {DEMO_URL} {REPO}",
+            f"""
+            cd {REPO}
+            git clean -fxd
+            git reset --hard HEAD
+            git pull
+            """,
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+
+    server.shell(
+        name=f"uv sync {DEMO_DIR}",
+        commands=[
+            f"""
+            cd {REPO}
+            uv venv -p3.12
+            . .venv/bin/activate
+            uv sync
+            """
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+
+
+main()
---
+++ kind-scripts-demo-4-resilience/12-deploy-demo-resilience.py
@@ -0,0 +1,253 @@
+"""
+Minimal recipe build for the resilience app.
+
+Warning: connection as user root.
+
+pyinfra -y -v --user root ${SERVER_NAME} 12-deploy-demo-resilience.py
+"""
+
+from pyinfra import host
+from pyinfra.facts.hardware import Ipv4Addrs
+from pyinfra.operations import files, python, server
+
+from common import log_callback
+from constants import GITS
+
+TOP_DIR = f"{GITS}/h3ni-demos/"
+SMO_URL = "http://127.0.0.1:8000"
+REGISTRY_URL = "http://host.docker.internal:5000"
+DEMO = f"{GITS}/h3ni-demos/4-resilience-demo"
+PROJECT = "demo4"
+GRAPH_NAME = "resilience-critical-graph"
+INTERNAL_IP = "host.docker.internal"
+
+
+def make_graph_delete_cmd(graph: str) -> str:
+    return f"curl -X DELETE {SMO_URL}/graphs/{graph}"
+
+
+def make_deploy_command(project: str, graph: str) -> str:
+    data = f'{{"artifact": "{REGISTRY_URL}/{project}/{graph}"}}'
+    cmd = (
+        f'curl -X POST "{SMO_URL}/project/{project}/graphs" '
+        '-H "Content-Type: application/json" '
+        f"--data '{data}'"
+    )
+    return cmd
+
+
+def make_graph_list_command(project: str) -> str:
+    return f'curl -X GET "{SMO_URL}/project/{project}/graphs"'
+
+
+def make_clusters_list_command() -> str:
+    return f'curl -X GET "{SMO_URL}/clusters/"'
+
+
+def main() -> None:
+    clean_installed_graphs()
+    change_hdag_file()
+    prepare_resilience()
+    deploy_on_smo()
+    check_graph_list()
+    check_clusters()
+    find_kubernetes_pods()
+    find_demo4()
+
+
+def clean_installed_graphs() -> None:
+    for name in (
+        "hello-world-graph",
+        "image-detection-graph",
+        "resilience-critical-graph",
+    ):
+        cmd = make_graph_delete_cmd(name)
+        server.shell(
+            name=f"Remove prior known graph {name}",
+            # commands=[(f"yes | {SMO_CLI} graph remove {name} || true")],
+            commands=[f"{cmd} || true"],
+            _get_pty=True,
+            _shell_executable="/bin/bash",
+        )
+
+
+def change_hdag_file() -> None:
+    files.put(
+        name="Put hdag file",
+        src="hdag.yaml",
+        dest="/root/gits/h3ni-demos/4-resilience-demo/hdag/hdag.yaml",
+        mode="644",
+    )
+
+
+def prepare_resilience() -> None:
+    ips = host.get_fact(Ipv4Addrs)
+    eth0 = ips["eth0"][0]
+    server.shell(
+        name="Fix image name and address",
+        commands=[
+            f"""
+            cd {DEMO}/hdag
+
+            perl -pi -e 's/127.0.0.1/{INTERNAL_IP}/g' hdag.yaml
+
+            perl -pi -e 's;version: '1.0.0';version: '0.1.0';g' hdag.yaml
+
+            cd {DEMO}/critical-service/templates
+            perl -pi -e 's/127.0.0.1/{eth0}/g' deployment.yaml
+            """
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+
+    result = server.shell(
+        name="Build and push images and artifacts",
+        commands=[
+            f"""
+            cd {TOP_DIR}
+            . .venv/bin/activate
+
+            cd {DEMO}
+            rm -fr dist
+            mkdir dist
+            rm -fr build
+            mkdir build
+
+            docker build -t "127.0.0.1:5000/critical-service-0.1.0" src/critical-service
+
+            docker push "127.0.0.1:5000/critical-service-0.1.0"
+
+            cp -a hdag build/
+            cp -a critical-service build/
+
+            hdarctl package tar -d dist build/hdag
+            hdarctl package tar -d dist build/critical-service
+
+            hdarctl push dist/resilience-critical-graph-0.1.0.tar.gz http://127.0.0.1:5000/demo4
+            hdarctl push dist/critical-service-0.1.0.tar.gz http://127.0.0.1:5000/demo4
+
+            """
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name=f"Show deploying {GRAPH_NAME}",
+        function=log_callback,
+        result=result,
+    )
+
+
+def deploy_on_smo() -> None:
+    cmd = make_deploy_command(PROJECT, GRAPH_NAME)
+    result = server.shell(
+        name=f"Deploy {GRAPH_NAME} to smo",
+        commands=[cmd],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name=f"Show deploying {GRAPH_NAME}",
+        function=log_callback,
+        result=result,
+    )
+
+
+def check_graph_list() -> None:
+    cmd = make_graph_list_command(PROJECT)
+    result = server.shell(
+        name="Get graph list",
+        commands=[cmd],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show graph list",
+        function=log_callback,
+        result=result,
+    )
+
+
+def check_clusters() -> None:
+    cmd = make_clusters_list_command()
+    result = server.shell(
+        name="Get clusters list",
+        commands=[cmd],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show clusters list",
+        function=log_callback,
+        result=result,
+    )
+
+
+def find_kubernetes_pods() -> None:
+    for context in (
+        # "karmada-host",
+        # "karmada-apiserver",
+        "member1",
+        # "member2",
+    ):
+        result = server.shell(
+            name=f"Find kubernetes pods in {context!r}",
+            commands=[
+                f"""
+            export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+            kubectl config use-context {context}
+
+            kubectl get pods -A
+            """
+            ],
+            _get_pty=True,
+            _shell_executable="/bin/bash",
+        )
+        python.call(
+            name=f"Show kubernetes pods in {context!r}",
+            function=log_callback,
+            result=result,
+        )
+
+
+def find_demo4() -> None:
+    result = server.shell(
+        name="Find demo4 in member1",
+        commands=[
+            """
+            export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+            kubectl config use-context member1
+
+            pod_name=$(kubectl get pods -n demo4 -o jsonpath='{.items[0].metadata.name}')
+            kubectl wait --for=condition=Ready pod/$pod_name -n demo4 --timeout=120s
+            kubectl port-forward -n demo4 $pod_name 8080:8080 &
+            sleep 2
+            counter=0
+            code=""
+            while [ $counter -lt 120 ]; do
+                code=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8080")
+                if [ "$code" -eq 200 ]; then
+                    break
+                fi
+                counter=$((counter + 2))
+                sleep 2
+            done
+
+            echo "curl -s http://localhost:8080"
+            curl -s http://localhost:8080
+            echo
+
+        """
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show demo4 response in member1",
+        function=log_callback,
+        result=result,
+    )
+
+
+main()
