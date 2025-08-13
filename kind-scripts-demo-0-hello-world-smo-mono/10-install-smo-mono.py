0efd420d6cffaab7d81eee3b1c30b851161e049a
Jerome Dumonteil <jerome.dumonteil@gmail.com>
2025-08-13 17:13:25 +0200

    add kind-scripts-demo-0-hello-world-smo-mono, ok until smo initialization

---
+++ kind-scripts-demo-0-hello-world-smo-mono/10-install-smo-mono.py
@@ -0,0 +1,206 @@
+"""
+Minimal recipe to install smo-monorepo
+
+Warning: connection as user root.
+
+pyinfra -y -v --user root ${SERVER_NAME} 10-install-smo-monorepo.py
+"""
+
+from pyinfra.operations import files, python, server
+
+from common import log_callback
+from constants import GITS
+
+SMO_MONO = "smo-monorepo"
+SMO_MONO_URL = (
+    "https://gitlab.eclipse.org/eclipse-research-labs/"
+    "nephele-project/opencall-2/h3ni/smo-monorepo.git"
+)
+BRANCH = "main"
+LOCAL_IP = "127.0.0.1"
+INTERNAL_IP = "host.docker.internal"
+REPO = f"{GITS}/{SMO_MONO}"
+SMO_CLI = "/usr/local/bin/smo-cli"
+
+
+def main() -> None:
+    remove_prior_smo_cli()
+    install_smo()
+    # make_smo_tests()
+    init_smo()
+    smo_cluster_sync()
+    smo_show_graph()
+
+
+def remove_prior_smo_cli() -> None:
+    files.file(
+        name=f"Remove {SMO_CLI} if exists",
+        path=SMO_CLI,
+        present=False,
+        force=True,
+    )
+
+
+def install_smo() -> None:
+    files.directory(
+        name=f"Create {GITS} directory",
+        path=GITS,
+    )
+
+    server.shell(
+        name=f"Clone/pull {SMO_MONO} source",
+        commands=[
+            f"[ -d {REPO} ] || git clone {SMO_MONO_URL} {REPO}",
+            f"""
+                cd {REPO}
+                git clean -fxd
+                git reset --hard HEAD
+                git pull
+                git checkout {BRANCH}
+            """,
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+
+    server.shell(
+        name="Build smo",
+        commands=[
+            f"""
+                cd {REPO}
+                uv venv --clear -p3.12
+                . .venv/bin/activate
+
+                uv sync
+            """
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+
+
+def make_smo_tests() -> None:
+    server.shell(
+        name="make smo tests",
+        commands=[
+            f"""
+                cd {REPO}
+                . .venv/bin/activate
+
+                make test
+            """
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+
+
+def init_smo() -> None:
+    server.shell(
+        name=f"Copy smo-cli to {SMO_CLI}",
+        commands=[
+            f"""
+                cp -f {REPO}/.venv/bin/smo-cli {SMO_CLI}
+            """
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+
+    server.shell(
+        name="Init smo",
+        commands=[
+            f"""
+                cd {REPO}
+                . .venv/bin/activate
+
+                export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+                kubectl config use-context karmada-apiserver
+
+                {SMO_CLI} init
+            """
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+
+    server.shell(
+        name="Replace IPs in smo config",
+        commands=[
+            f"""
+                cd {REPO}
+                . .venv/bin/activate
+
+                # export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+                # kubectl config use-context karmada-apiserver
+                # kubectl get services --all-namespaces
+
+                # this is the cluster NodePort ip:
+                # prom_ip=$(kubectl get service prometheus-kube-prometheus-prometheus -n monitoring -o jsonpath='{{.spec.clusterIP}}')
+
+                # get grafana address from outside cluster
+                export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+                kubectl config use-context karmada-host
+
+                kind_node_ip=$(docker inspect -f '{{{{range.NetworkSettings.Networks}}}}{{{{.IPAddress}}}}{{{{end}}}}' karmada-host-control-plane)
+                grafana_port=$(kubectl get svc -n monitoring prometheus-grafana -o jsonpath='{{.spec.ports[?(@.name=="http-web")].nodePort}}')
+
+                cd /root/.smo
+                perl -pi -e "s;  host: http://.*;  host: http://${{kind_node_ip}}:${{grafana_port}};g" config.yaml
+            """
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+
+
+def smo_cluster_sync() -> None:
+    result = server.shell(
+        name="Exec smo-cli cluster sync",
+        commands=[
+            f"""\
+                cd {REPO}
+                . .venv/bin/activate
+
+                export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+                kubectl config use-context karmada-apiserver
+
+                smo-cli cluster sync
+            """
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+    python.call(
+        name="Show smo-cli cluster sync",
+        function=log_callback,
+        result=result,
+    )
+
+
+def smo_show_graph() -> None:
+    result = server.shell(
+        name="Exec smo-cli graph list",
+        commands=[
+            f"""\
+                cd {REPO}
+                . .venv/bin/activate
+
+                export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+                kubectl config use-context karmada-apiserver
+
+                echo "graph list:"
+                smo-cli graph list
+            """
+        ],
+        _shell_executable="/bin/bash",
+        _get_pty=True,
+    )
+    python.call(
+        name="Show smo-cli graph list",
+        function=log_callback,
+        result=result,
+    )
+
+
+main()
---
+++ kind-scripts-demo-0-hello-world-smo-mono/12-deploy-smo-demo-hello-world.py
@@ -0,0 +1,221 @@
+"""
+Minimal recipe build for the hello-world app.
+
+Warning: connection as user root.
+
+pyinfra -y -v --user root ${SERVER_NAME} 12-deploy-demo-hello-world.py
+"""
+
+from pyinfra import host
+from pyinfra.facts.hardware import Ipv4Addrs
+from pyinfra.operations import python, server
+
+from common import log_callback
+from constants import GITS
+
+TOP_DIR = f"{GITS}/h3ni-demos/"
+SMO_URL = "http://127.0.0.1:8000"
+REGISTRY_URL = "http://host.docker.internal:5000"
+DEMO = f"{GITS}/h3ni-demos/0-hello-world-demo"
+PROJECT = "demo0"
+GRAPH_NAME = "hello-world-graph"
+INTERNAL_IP = "host.docker.internal"
+
+
+def make_graph_delete_cmd(graph: str) -> str:
+    return f"curl -X DELETE {SMO_URL}/graphs/{graph}"
+
+
+def make_deploy_command(project: str, graph: str) -> str:
+    data = f'{{"artifact": "{REGISTRY_URL}/{project}/{graph}"}}'
+    cmd = (
+        f'curl -X POST "{SMO_URL}/project/{project}/graphs" '
+        '-H "Content-Type: application/json" '
+        f"--data '{data}'"
+    )
+    return cmd
+
+
+def make_graph_list_command(project: str) -> str:
+    return f'curl -X GET "{SMO_URL}/project/{project}/graphs"'
+
+
+def make_clusters_list_command() -> str:
+    return f'curl -X GET "{SMO_URL}/clusters/"'
+
+
+def main() -> None:
+    clean_installed_graphs()
+    prepare_hello_world()
+    deploy_on_smo()
+    check_graph_list()
+    check_clusters()
+    find_kubernetes_pods()
+    find_demo0()
+
+
+def clean_installed_graphs() -> None:
+    for name in ("hello-world-graph", "image-detection-graph"):
+        cmd = make_graph_delete_cmd(name)
+        server.shell(
+            name=f"Remove prior known graph {name}",
+            # commands=[(f"yes | {SMO_CLI} graph remove {name} || true")],
+            commands=[f"{cmd} || true"],
+            _get_pty=True,
+            _shell_executable="/bin/bash",
+        )
+
+
+def prepare_hello_world() -> None:
+    ips = host.get_fact(Ipv4Addrs)
+    eth0 = ips["eth0"][0]
+    server.shell(
+        name="Fix image name and address",
+        commands=[
+            f"""
+            cd {DEMO}/hdag
+
+            perl -pi -e 's/127.0.0.1/{INTERNAL_IP}/g' hdag.yaml
+
+            perl -pi -e 's;test/hello-world;demo0/hello-world;g' hdag.yaml
+
+            perl -pi -e 's;version: '1.0.0';version: '0.1.0';g' hdag.yaml
+
+            cd {DEMO}/hello-world/templates
+            # perl -pi -e 's/127.0.0.1/{INTERNAL_IP}/g' deployment.yaml
+            # perl -pi -e 's/127.0.0.1/localhost/g' deployment.yaml
+            perl -pi -e 's/127.0.0.1/{eth0}/g' deployment.yaml
+            """
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+
+    server.shell(
+        name="Prepare hello-world-graph",
+        commands=[
+            f"""
+            cd {TOP_DIR}
+            . .venv/bin/activate
+
+            cd {DEMO}
+            inv prepare
+            """
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+
+
+def deploy_on_smo() -> None:
+    cmd = make_deploy_command(PROJECT, GRAPH_NAME)
+    result = server.shell(
+        name="Deploy hello-world-graph to smo",
+        commands=[cmd],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show deploying hello-world-graph",
+        function=log_callback,
+        result=result,
+    )
+
+
+def check_graph_list() -> None:
+    cmd = make_graph_list_command(PROJECT)
+    result = server.shell(
+        name="Get graph list",
+        commands=[cmd],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show graph list",
+        function=log_callback,
+        result=result,
+    )
+
+
+def check_clusters() -> None:
+    cmd = make_clusters_list_command()
+    result = server.shell(
+        name="Get clusters list",
+        commands=[cmd],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show clusters list",
+        function=log_callback,
+        result=result,
+    )
+
+
+def find_kubernetes_pods() -> None:
+    for context in (
+        # "karmada-host",
+        # "karmada-apiserver",
+        "member1",
+        # "member2",
+    ):
+        result = server.shell(
+            name=f"Find kubernetes pods in {context!r}",
+            commands=[
+                f"""
+            export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+            kubectl config use-context {context}
+
+            kubectl get pods -A
+            """
+            ],
+            _get_pty=True,
+            _shell_executable="/bin/bash",
+        )
+        python.call(
+            name=f"Show kubernetes pods in {context!r}",
+            function=log_callback,
+            result=result,
+        )
+
+
+def find_demo0() -> None:
+    result = server.shell(
+        name="Find demo0 in member1",
+        commands=[
+            """
+            export KUBECONFIG="/root/.kube/karmada-apiserver.config"
+            kubectl config use-context member1
+
+            pod_name=$(kubectl get pods -n demo0 -o jsonpath='{.items[0].metadata.name}')
+            kubectl wait --for=condition=Ready pod/$pod_name -n demo0 --timeout=120s
+            kubectl port-forward -n demo0 $pod_name 8080:8080 &
+            sleep 2
+            counter=0
+            code=""
+            while [ $counter -lt 120 ]; do
+                code=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8080")
+                if [ "$code" -eq 200 ]; then
+                    break
+                fi
+                counter=$((counter + 2))
+                sleep 2
+            done
+
+            echo "curl -s http://localhost:8080"
+            curl -s http://localhost:8080
+            echo
+
+        """
+        ],
+        _get_pty=True,
+        _shell_executable="/bin/bash",
+    )
+    python.call(
+        name="Show demo0 response in member1",
+        function=log_callback,
+        result=result,
+    )
+
+
+main()
